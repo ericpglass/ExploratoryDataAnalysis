9a237398ece494d8ba2d85781ad1403ad7e040df
ericpglass <ericpglass@hotmail.com>
2014-12-20 18:46:35 -0700

    Project2

---
+++ Project2/plot1.R
@@ -0,0 +1,18 @@
+# Load provided datasets 
+NEI <- readRDS("~/coursera/EDA/summarySCC_PM25.rds")
+SCC <- readRDS("~/coursera/EDA/Source_Classification_Code.rds")
+
+# Aggregate by sum the total emissions per year
+aggTotals <- aggregate(Emissions ~ year,NEI, sum)
+
+png("plot1.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+        (aggTotals$Emissions)/10^6,
+        names.arg=aggTotals$year,
+        xlab="Year",
+        ylab="PM2.5 Emissions (10^6 Tons)",
+        main="Total PM2.5 Emissions From All US Sources"
+)
+
+dev.off()
---
+++ Project2/plot2.R
@@ -0,0 +1,22 @@
+# Load provided datasets
+NEI <- readRDS("~/coursera/EDA/summarySCC_PM25.rds")
+SCC <- readRDS("~/coursera/EDA/Source_Classification_Code.rds")
+
+# Subset NEI data by Baltimore's fip
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+# Aggregate using sum Baltimore's emissions data by year
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot2.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+        aggTotalsBaltimore$Emissions,
+        names.arg=aggTotalsBaltimore$year,
+        xlab="Year",
+        ylab="PM2.5 Emissions (Tons)",
+        main="Total PM2.5 Emissions From all Baltimore City Sources"
+)
+
+dev.off()
+
